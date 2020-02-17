2db0507c336495369c2efea0c8f1948b6c7d6160
nikki003 <nikkibhalodiya03@gmail.com>
2020-02-18 00:05:17 +0530

    established connection and created AddPerson component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import AddPerson  from './Components/AddPerson';
+import PersonList from './Components/PersonList';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+export default function App() {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello</h2>
+      <AddPerson />
+      <PersonList />
     </div>
   );
 }
-
-export default App;
